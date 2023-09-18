db5902a23b47d8fc78d3772770369e1e2708cdeb
vc-vishakha <vishakha.tak@viitor.cloud>
2023-09-18 15:03:35 +0530

    Implemented handlerbar template for health check api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 // const bodyParser = require('body-parser')
+const { engine } = require('express-handlebars');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -12,8 +14,29 @@ initiateMongoServer();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/health', (req, res) => {
-    res.send('Hello World!!');
+// Configure Express Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// Set views directory
+app.set('views', path.join(__dirname, 'templates', 'emails'));
+
+app.get('/health', async (req, res) => {
+    try {
+        const healthcheck = {
+            uptime: process.uptime(),
+            message: 'OK',
+            timestamp: Date.now()
+        };
+        // Render email template
+        const html = await res.render('welcome', { healthcheck });
+
+        // Send email using Nodemailer
+        // (Nodemailer setup will be covered in the next section)
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // routes
